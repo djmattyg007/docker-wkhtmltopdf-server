37a51916ca08f34f7c0f6cc877f5840ddced43f9
Matthew Gamble <git@matthewgamble.net>
2017-07-03 09:02:22 +1000

    Whoops

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -6,7 +6,7 @@ set -e
 source /root/functions.sh
 
 echo "Installing wkhtmltopdf"
-pacman -S --noconfirm --noprogressbar --color=never wkhtmltopdf xvfb
+pacman -S --noconfirm --noprogressbar --color=never wkhtmltopdf xorg-server-xvfb
 
 # Cleanup
 pacman_cleanup
